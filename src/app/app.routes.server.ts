bc60c12f5f8f555f50840e8d85ddd0e2c93ae639
Gonzalo <cacalo219@hotmail.com>
2025-01-14 14:04:20 -0300

    Fix: Added client rendering to withparams/:params

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'withparams/:params',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
